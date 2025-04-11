3d7b69c53af2c6701d77c46628b00fb8e9d0f465
yahaya <developpeur.yb @gmail.com>
2025-04-11 20:26:49 +0200

    ADD single page layout ADD main page file ADD the global css file and content

---
+++ dev/portfolio/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+/**
+ * Page content
+ * @constructor
+ */
+export default function PortfolioPage(): React.ReactElement {
+    return (
+        <p>Bonjour</p>
+    )
+}
