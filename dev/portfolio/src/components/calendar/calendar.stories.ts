1adf76a4cebc74d29b3735c12a87bb9e0ace63ee
yahaya <developpeur.yb @gmail.com>
2025-04-12 16:53:18 +0200

    ADD calendar component ADD calendar component's style ADD calendar component's stories MODIFY size variables in global css

---
+++ dev/portfolio/src/components/calendar/calendar.stories.ts
@@ -0,0 +1,30 @@
+import {StoryObj} from "@storybook/react"
+import {AvailabilityMap, Calendar} from "@/components/calendar/calendar"
+import {Meta} from "@storybook/react"
+
+type CalenderStory = StoryObj<typeof Calendar>
+
+const meta: Meta = {
+    title: "UI/Calendar",
+    component: Calendar
+}
+
+export default meta
+
+// stories
+
+export const DefaultCalendar: CalenderStory = {
+    args: {
+        startMonth: 4,
+        year: 2025,
+        availabilityGetter: async (): Promise<AvailabilityMap> => {
+            return {
+                1: [
+                    {start: "8H", end: "10H"},
+                    {start: "12H", end: "14H"},
+                ]
+            }
+        },
+        onAvailabilityChosen: () => console.log("availability chosen")
+    }
+}
