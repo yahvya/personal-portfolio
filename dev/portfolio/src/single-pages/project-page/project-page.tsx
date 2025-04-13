d8c4a563a76f405e17959395337dbde40ec5dd15
yahaya <developpeur.yb @gmail.com>
2025-04-13 18:24:33 +0200

    FIX env example cv link MODIFY project page , list page component to add a show status argument MODIFY project page to allow going back

--- dev/portfolio/src/single-pages/project-page/project-page.tsx
+++ dev/portfolio/src/single-pages/project-page/project-page.tsx
@@ -1,10 +1,11 @@
 import "./project-page.scss"
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 import {useQuery} from "react-query"
 import {ProjectElement} from "@/components/project-element/project-element"
 import {ReactTyped} from "react-typed";
 import {typingConfig} from "@/single-pages/project-page/config"
 import {motion} from "motion/react"
+import classNames from "classnames";
 
 /**
  * Project description
@@ -26,14 +27,25 @@ interface ProjectDescriptor {
  * @constructor
  */
 function ProjectDetailsPage(
-    {project}:
-    { project: ProjectDescriptor }
+    {
+        project,
+        onExit
+    }:
+    {
+        project: ProjectDescriptor,
+        onExit: (project: ProjectDescriptor) => void
+    }
 ): React.ReactElement {
     const [ typingIndex, setTypingIndex ] = useState(0)
 
     return (
         <div className="project-details-page">
             <div className="container">
+                <p
+                    className="close basic-text-bold"
+                    onClick={() => onExit(project)}
+                >&lt;- Fermer</p>
+
                 <p className="large-text-bold">
                     <ReactTyped
                         {...typingConfig}
@@ -93,13 +105,36 @@ function ProjectDetailsPage(
                             <p className="basic-text-bold">Réalisés</p>
                             <ul>
                                 {
-                                    project.tasks.map((task: string): React.ReactElement => (
-                                        <li>{task}</li>
+                                    project.tasks.map((task: string, index: number): React.ReactElement => (
+                                        <li key={index}>{task}</li>
                                     ))
                                 }
                             </ul>
                         </div>
                     </motion.div>
+                }
+            </div>
+
+            <div className="images-container">
+                {
+                    typingIndex > 0 &&
+                    (<>
+                        {
+                            project.projectImagesLinks.map((imageLink: string, index: number): React.ReactElement => (
+                                <motion.img
+                                    key={index}
+                                    initial={{
+                                        opacity: 0
+                                    }}
+                                    whileInView={{
+                                        opacity: 1
+                                    }}
+                                    src={imageLink}
+                                    alt="Project image"
+                                />
+                            ))
+                        }
+                    </>)
                 }
             </div>
         </div>
@@ -108,22 +143,32 @@ function ProjectDetailsPage(
 
 /**
  * Project pages
+ * @param show If true show list of hide it on false
  * @param projects projects to show
  * @param onProjectChose On project chose
  * @constructor
  */
 function ProjectListPage(
     {
+        show,
         projects,
         onProjectChose
     }:
     {
+        show: boolean,
         projects: ProjectDescriptor[],
         onProjectChose: (chosenProject: ProjectDescriptor) => any
     }
 ): React.ReactElement {
+    const classnames: string[] = [ "project-page" ]
+
+    if (!show)
+        classnames.push("hide")
+
+    const classnamesList: string = classNames(...classnames)
+
     return (
-        <div className="project-page">
+        <div className={classnamesList}>
             {
                 projects.map((project: ProjectDescriptor, index: number): React.ReactElement => (
                     <ProjectElement
@@ -160,18 +205,32 @@ export function ProjectPage(): React.ReactElement {
     })
 
     const [ projectToShowDetails, setProjectToShowDetails ] = useState<ProjectDescriptor | null>(null)
+    const [ showProjects, setShowProjects ] = useState<boolean>(true)
+
+    useEffect(() => {
+        if (projectToShowDetails)
+            setShowProjects(false)
+        else
+            setShowProjects(true)
+    }, [ projectToShowDetails ])
+
+    // configs
 
     const projects: ProjectDescriptor[] = (data ?? []) as ProjectDescriptor[]
 
     return (
         <>
+            <ProjectListPage
+                show={showProjects}
+                projects={projects}
+                onProjectChose={(projectDetails: ProjectDescriptor) => setProjectToShowDetails(projectDetails)}
+            />
             {
-                projectToShowDetails ?
-                    <ProjectDetailsPage project={projectToShowDetails}/> :
-                    <ProjectListPage
-                        projects={projects}
-                        onProjectChose={(projectDetails: ProjectDescriptor) => setProjectToShowDetails(projectDetails)}
-                    />
+                projectToShowDetails &&
+                <ProjectDetailsPage
+                    project={projectToShowDetails}
+                    onExit={() => setProjectToShowDetails(null)}
+                />
             }
         </>
     )
