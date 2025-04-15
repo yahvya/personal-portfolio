0bd8cb3882cba31197c77b2d8f3db7bc59fb3507
yahaya <developpeur.yb @gmail.com>
2025-04-15 10:11:09 +0200

    ADD technical stack design and structure elements

--- dev/portfolio/src/single-pages/technical-stack-page/technical-stack-page.tsx
+++ dev/portfolio/src/single-pages/technical-stack-page/technical-stack-page.tsx
@@ -1,11 +1,12 @@
-import React from "react"
+import React, {useRef} from "react"
 import "./technical-stack-page.scss"
 import {useQuery} from "react-query"
 import {requestTechnicalStackElements} from "@/api-handlers/personal-github/requests"
 import {StackElementDescriptor, StackElementsFetchResponse} from "@/api-handlers/personal-github/responses.dto"
-import {motion} from "motion/react"
+import {motion, useInView} from "motion/react"
 import {StackElement} from "@/components/stack-element/stack-element"
 import {opacityGreatSlideUpAnimation} from "@/animations/motion-common";
+import {ReactTyped} from "react-typed";
 
 /**
  * Technical stack page
@@ -13,6 +14,9 @@ import {opacityGreatSlideUpAnimation} from "@/animations/motion-common";
  */
 export function TechnicalStackPage(): React.ReactElement {
     // states
+
+    const ref = useRef<HTMLDivElement | null>(null)
+    const isInView = useInView(ref, {once: false})
 
     const {data} = useQuery({
         queryKey: "technical-stack",
@@ -23,6 +27,19 @@ export function TechnicalStackPage(): React.ReactElement {
 
     return (
         <div className="technical-stack-page">
+            <div className="page-title" ref={ref}>
+                {
+                    isInView && (
+                        <ReactTyped
+                            strings={[ "Une idée de ma stack technique ?" ]}
+                            showCursor={false}
+                            className="large-text-bold"
+                            typeSpeed={30}
+                        />
+                    )
+                }
+            </div>
+
             <div className="stack-elements-container">
                 {
                     technicalStackElements.map((stackElementConfig: StackElementDescriptor, index: number): React.ReactElement => (
